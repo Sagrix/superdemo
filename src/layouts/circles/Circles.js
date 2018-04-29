a5957dcfc3e619cadd24e865296a9f6db4380e5d
Tomisin <tomisinjenrola@gmail.com>
2018-04-29 10:12:28 -0230

    created copmonents for mycircle

--- src/layouts/circles/Circles.js
+++ src/layouts/circles/Circles.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Table, Input, Icon, Button, Popconfirm } from 'antd'
+import MyCircle from './MyCircle'
+import MemberCircles from './MemberCircles'
 
 
 class Circles extends Component {
@@ -11,10 +13,27 @@ class Circles extends Component {
 
   render() {
     return(
-      <Row>
-        <Col>My</Col>
-        <Col>Circles</Col>
-      </Row>
+      <div>
+        <Row>
+          <Col span={12} offset={6}>
+            <MyCircle/>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col span={12} offset={6}>
+            <h1>Participating Circles</h1>
+            <h4>People who have entrusted you as their substitute decision maker.</h4>
+          </Col>
+        </Row>
+
+        {/* <Row>
+          <Col span={12} offset={6}>
+            <h1>Forbidden</h1>
+            <h4>People who have do not want to make decisions on your behalf.</h4>
+          </Col>
+        </Row> */}
+      </div>
     )
   }
 
---
+++ src/layouts/circles/MyCircle.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react'
+import { Table, Input, Icon, Button, Popconfirm } from 'antd'
+import EditableCell from './EditableCell'
+
+
+class MyCircle extends Component {
+
+  constructor(props, { authData }) {
+    super(props)
+    authData = this.props
+    
+    this.columns = [{
+      title: 'name',
+      dataIndex: 'name',
+      width: '30%',
+      render: (text, record) => (
+        <EditableCell
+          value={text}
+          onChange={this.onCellChange(record.key, 'name')}
+        />
+      ),
+    }, {
+      title: 'age',
+      dataIndex: 'age',
+    }, {
+      title: 'address',
+      dataIndex: 'address',
+    }, {
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (text, record) => {
+        return (
+          this.state.dataSource.length > 1 ?
+          (
+            <Popconfirm title="Sure to delete?" onConfirm={() => this.onDelete(record.key)}>
+              <a href="javascript:;">Delete</a>
+            </Popconfirm>
+          ) : null
+        );
+      },
+    }];
+
+    this.state = {
+      dataSource: [{
+        key: '0',
+        name: 'Edward King 0',
+        age: '32',
+        address: 'London, Park Lane no. 0',
+      }, {
+        key: '1',
+        name: 'Edward King 1',
+        age: '32',
+        address: 'London, Park Lane no. 1',
+      }],
+      count: 2,
+    };
+  }
+
+  onCellChange = (key, dataIndex) => {
+    return (value) => {
+      const dataSource = [...this.state.dataSource];
+      const target = dataSource.find(item => item.key === key);
+      if (target) {
+        target[dataIndex] = value;
+        this.setState({ dataSource });
+      }
+    };
+  }
+  onDelete = (key) => {
+    const dataSource = [...this.state.dataSource];
+    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
+  }
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count,
+      name: `Edward King ${count}`,
+      age: 32,
+      address: `London, Park Lane no. ${count}`,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  }
+
+  render() {
+    const { dataSource } = this.state;
+    const columns = this.columns;
+    return(
+      <div>
+        <h1>My Circle</h1>
+        <h4>People you trust as substitute decision makers of your health care choices and data</h4>  
+        <Button className="editable-add-btn" onClick={this.handleAdd}>Add</Button>
+        <Table bordered dataSource={dataSource} columns={columns} />
+      </div>
+    )
+  }
+
+}
+
+export default MyCircle
