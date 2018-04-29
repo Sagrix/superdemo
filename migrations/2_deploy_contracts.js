f588a1c868a15f4c8eb0b3f7b0627d4010cfce28
Tomisin <tomisinjenrola@gmail.com>
2018-04-29 07:37:47 -0230

    Add SimpleAHD deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Destructible = artifacts.require("./zeppelin/lifecycle/Destructible.sol");
 var Authentication = artifacts.require("./Authentication.sol");
+var SimpleAHD = artifacts.require('./SimpleAHD.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
@@ -8,4 +9,5 @@ module.exports = function(deployer) {
   deployer.deploy(Destructible);
   deployer.link(Destructible, Authentication);
   deployer.deploy(Authentication);
+  deployer.deploy(SimpleAHD);
 };
