fdf6250753e20f8449b0232f1b78d803fbf82aa9
Tomisin <tomisinjenrola@gmail.com>
2018-05-02 19:55:49 -0400

    Started filling up preferences page

--- src/layouts/preferences/Preferences.js
+++ src/layouts/preferences/Preferences.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'antd'
+import { Row, Col, Divider, Form, InputNumber, Button, Radio } from 'antd'
+const FormItem = Form.Item;
+const RadioGroup = Radio.Group;
 
 
 class Preferences extends Component {
@@ -9,12 +11,41 @@ class Preferences extends Component {
     authData = this.props
   }
 
+  onChange(value) {
+    console.log('changed', value);
+    // call smart contract function- setRequiredVotes
+  }
+  
+
   render() {
     return(
       <Row>
         <Col span={12} offset={6}>
           <h1>My Healthcare Preferences</h1>
-          <h3>Decisions you want made when you are unable to make them yourself.</h3>
+          <h3>Decisions you want made on your behalf when you are unable to make them for yourself.</h3>
+          
+          <Divider />
+
+          <Form layout="inline">
+            <FormItem label="Min. Votes Required">
+              <InputNumber min={1} max={10} defaultValue={3} onChange={this.onChange} />
+            </FormItem>
+            <FormItem>
+              <Button type="primary" htmlType="submit">Update</Button>
+            </FormItem>
+          </Form>
+
+          <Divider />
+
+          <Form>
+            <FormItem label="Medical treatment for the primary purpose of research:">
+              <RadioGroup>
+                <Radio value="a">Yes</Radio>
+                <Radio value="b">No</Radio>
+              </RadioGroup>
+            </FormItem>
+          </Form>
+
         </Col>
       </Row>
 
